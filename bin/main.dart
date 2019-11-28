2ee94c66eb59677c9537ee9018d1651ab43d45b6
kapiljhajhria <Aerospace44>
2019-11-28 10:09:34 +0530

    still working for step 1

--- bin/main.dart
+++ bin/main.dart
@@ -3,13 +3,33 @@ import 'day39.dart';
 // Find the best move given the state of Towers of Hanoi board
 
 int bestHanoiMove(List<List> towers) {
-  int numberOfMovesToWin =0;
+  int numberOfMovesToWin = 0;
   List<List> allPossibleMoves = hanoiPossibleMoves(towers);
   print(allPossibleMoves);
-  if(winningMoveNumber(hanoiPossibleMoves(towers))==null){
-    numberOfMovesToWin++;
-    
+  if (winningMoveNumber(allPossibleMoves) == null) {
+    for (int i = 0; i < allPossibleMoves.length; i++) {
+      List<List> allPossibleMovesAgain =
+          hanoiPossibleMoves(allPossibleMoves[i]);
+      if (winningMoveNumber(allPossibleMovesAgain[i]) != null) {
+        return winningMoveNumber(allPossibleMoves[i]);
+      }
+    }
   }
+
+  // if(winningMoveNumber(allPossibleMoves)==null){
+  //   numberOfMovesToWin++;
+  //   print('numberOfMovesToWin:$numberOfMovesToWin');
+  //   allPossibleMoves.forEach((oneOfTheMovesOutput){
+  //     if(bestHanoiMove(oneOfTheMovesOutput)!=null){
+  //       //if any of the possible moves result in wiining game after one more move,
+  //       //then do this
+  //       return int;
+  //     }
+  //     return bestHanoiMove(oneOfTheMovesOutput);
+  //   });
+  // }else{
+  //   return winningMoveNumber(allPossibleMoves);
+  // }
   return winningMoveNumber(allPossibleMoves);
 }
 
@@ -17,7 +37,7 @@ int winningMoveNumber(List<List> possibleMoves) {
   for (int i = 0; i < possibleMoves.length; i++) {
     //go through every possible sceanrio
     if (winningStack(possibleMoves[i][2])) {
-      print('here in winningMove function');
+      // print('here in winningMove function');
       return i;
     }
   }
@@ -27,7 +47,7 @@ int winningMoveNumber(List<List> possibleMoves) {
 
 bool winningStack(List stack) {
   for (int i = 0; i < stack.length; i++) {
-    if (stack[i] != i+1) {
+    if (stack[i] != i + 1) {
       return false;
     }
   }
@@ -35,11 +55,11 @@ bool winningStack(List stack) {
 }
 
 main() {
-  print(winningStack([1,2,3,4]));
+  print(winningStack([1, 2, 3, 4]));
   List<List> currentGameInstance = [
-    [2],
     [1],
-    [3, 4]
+    [],
+    [2, 3, 4]
   ];
   print(bestHanoiMove(currentGameInstance));
 }
