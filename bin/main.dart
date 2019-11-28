2136201e3740ebc392164f4478e57bc8d44b605f
kapiljhajhria <Aerospace44>
2019-11-28 09:22:31 +0530

    step 1 completed

--- bin/main.dart
+++ bin/main.dart
@@ -2,24 +2,36 @@ import 'day39.dart';
 // Challenge
 // Find the best move given the state of Towers of Hanoi board
 
-bool bestHanoiMove(List<List> towers){
-
-return winningMove(hanoiPossibleMoves(towers));
+bool bestHanoiMove(List<List> towers) {
+  return winningMove(hanoiPossibleMoves(towers));
 }
 
-bool winningMove( List<List> possibleMoves){
-   return possibleMoves.any((oneOfTheMove)=>oneOfTheMove.any((stack)=>stack==[1,2,3,4]));
+bool winningMove(List<List> possibleMoves) {
+  for (int i = 0; i < possibleMoves.length; i++) {
+    //go through every possible sceanrio
+    if (winningStack(possibleMoves[i][2])) {
+      print('here in winningMove function');
+      return true;
+    }
+  }
 
+  return false;
 }
 
-bool winningStack(List stack){
-  for(int i=1;i<=stack.length;i++){
-    if(stack!=i){
+bool winningStack(List stack) {
+  for (int i = 0; i < stack.length; i++) {
+    if (stack[i] != i+1) {
       return false;
     }
   }
   return true;
 }
+
 main() {
-  print(bestHanoiMove([[],[1],[2,3,4]]));
+  print(winningStack([1,2,3,4]));
+  print(bestHanoiMove([
+    [],
+    [1],
+    [2, 3, 4]
+  ]));
 }
