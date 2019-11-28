501f91cf03442491b4f28d4c2abc800b1bcfe09d
kapiljhajhria <Aerospace44>
2019-11-28 10:56:38 +0530

    step 1 as per ex

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,34 @@
 import 'day39.dart';
+
 // Challenge
 // Find the best move given the state of Towers of Hanoi board
+List<List> oneMoveToWin(List<List> towers) {
+  List<List> allPossibleMoves = hanoiPossibleMoves(towers);
+  print(allPossibleMoves);
+  return allPossibleMoves[winningMoveNumber(allPossibleMoves)];
+}
 
+
+int winningMoveNumber(List<List> possibleMoves) {
+  for (int i = 0; i < possibleMoves.length; i++) {
+    //go through every possible sceanrio
+    if (isThisWinningMove(possibleMoves[i])) {
+      // print('here in winningMove function');
+      return i;
+    }
+  }
+
+  return null;
+}
+
+bool isThisWinningMove(List possibleMov) {
+  if (possibleMov[0].isEmpty && possibleMov[1].isEmpty) {
+    return true;
+  }
+  return false;
+}
+
+//------------------------ends here---------------
 int bestHanoiMove(List<List> towers) {
   int numberOfMovesToWin = 0;
   List<List> allPossibleMoves = hanoiPossibleMoves(towers);
@@ -22,21 +49,10 @@ int bestHanoiMove(List<List> towers) {
   return winningMoveNumber(allPossibleMoves);
 }
 
-int winningMoveNumber(List<List> possibleMoves) {
-  for (int i = 0; i < possibleMoves.length; i++) {
-    //go through every possible sceanrio
-    if (winningStack(possibleMoves[i][2])) {
-      // print('here in winningMove function');
-      return i;
-    }
-  }
-
-  return null;
-}
 
 bool winningStack(List stack) {
   //bug fix, otherwise even [1,2,3] , [1,2] etc are also counted as winning tower
-  if(stack.length!=4){
+  if (stack.length != 4) {
     return false;
   }
   for (int i = 0; i < stack.length; i++) {
@@ -50,9 +66,9 @@ bool winningStack(List stack) {
 main() {
   print(winningStack([1, 2, 3, 4]));
   List<List> currentGameInstance = [
-    [2 ,1],
+    [1],
     [],
-    [3, 4]
+    [2, 3, 4]
   ];
   print(bestHanoiMove(currentGameInstance));
 }
--- test/Day_40_Morning_Challenge_test.dart
+++ test/Day_40_Morning_Challenge_test.dart
@@ -2,6 +2,9 @@ import 'package:test/test.dart';
 import '../bin/main.dart';
 
 void main() {
-  test('Test Case 1', () {
+  test('1 move to win', () {
+    expect(oneMoveToWin([[2 ,1],[],[3, 4]]), null);//required 2 move to win
+    expect(oneMoveToWin([[1],[],[2, 3, 4]]), 1);
+    
   });
 }
