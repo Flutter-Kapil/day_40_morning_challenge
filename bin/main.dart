f5a2dd28d0b70907a3cb0007279b9bf292a2766e
kapiljhajhria <Aerospace44>
2019-11-28 11:44:54 +0530

    refactor and added one more test case

--- bin/main.dart
+++ bin/main.dart
@@ -19,6 +19,7 @@ List<List> twoMoveToWin(List<List> towers) {
   }
   return allPossibleMoves[winningMoveIndex];
 }
+//-------------
 List<List> oneMoveToWin(List<List> towers) {
   List<List> allPossibleMoves = hanoiPossibleMoves(towers);
   print(allPossibleMoves);
@@ -31,61 +32,17 @@ List<List> oneMoveToWin(List<List> towers) {
   }
   return null;
 }
-
-
-// int winningMoveIndex(List<List> possibleMoves) {
-//   for (int i = 0; i < possibleMoves.length; i++) {
-//     //go through every possible sceanrio
-//     if (isThisWinningMove(possibleMoves[i])) {
-//       // print('here in winningMove function');
-//       return i;
-//     }
-//   }
-
-//   return null;
-// }
+//------------
 
 bool isThisWinningMove(List possibleMov) {
   return (possibleMov[0].isEmpty && possibleMov[1].isEmpty);
 }
 
-//------------------------ends here---------------
-// int bestHanoiMove(List<List> towers) {
-//   int numberOfMovesToWin = 0;
-//   List<List> allPossibleMoves = hanoiPossibleMoves(towers);
-//   print(allPossibleMoves);
-
-  //-----------
-//   if (winningMoveIndex(allPossibleMoves) == null) {
-//     for (int i = 0; i < allPossibleMoves.length; i++) {
-//       List<List> allPossibleMovesAgain =
-//           hanoiPossibleMoves(allPossibleMoves[i]);
-//       if (winningMoveIndex(allPossibleMovesAgain) != null) {
-//         print('wining move in 2nd case is $allPossibleMovesAgain');
-//         print(winningMoveIndex(allPossibleMoves));
-//         return winningMoveIndex(allPossibleMoves);
-//       }
-//     }
-//   }
-//   return winningMoveIndex(allPossibleMoves);
-// }
 
 
-bool winningStack(List stack) {
-  //bug fix, otherwise even [1,2,3] , [1,2] etc are also counted as winning tower
-  if (stack.length != 4) {
-    return false;
-  }
-  for (int i = 0; i < stack.length; i++) {
-    if (stack[i] != i + 1) {
-      return false;
-    }
-  }
-  return true;
-}
+
 
 main() {
-  print(winningStack([1, 2, 3, 4]));
   List<List> currentGameInstance = [
     [1],
     [2],
--- test/Day_40_Morning_Challenge_test.dart
+++ test/Day_40_Morning_Challenge_test.dart
@@ -4,8 +4,8 @@ import '../bin/main.dart';
 void main() {
   test('1 move to win', () {
     expect(oneMoveToWin([[2 ,1],[],[3, 4]]), null);//requires 2 move to win
-    expect(oneMoveToWin([[1],[],[2, 3, 4]]), [[], [], [1, 2, 3, 4]]//requires 1 move to win
-);
+    expect(oneMoveToWin([[1],[],[2, 3, 4]]), [[], [], [1, 2, 3, 4]]);//requires 1 move to win
+    expect(oneMoveToWin([[1],[],[2, 3, 4, 5]]), [[], [], [1, 2, 3, 4,5]]);//requires 1 move to win
     
   });
 }
